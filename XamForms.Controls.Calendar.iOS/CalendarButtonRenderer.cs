4b385b0d1733282273ca722aa3e9e9a159371d1f
Rebecca <rebeccaXam@gmail.com>
2017-07-09 17:56:46 +0200

    Add Text to Special Date in iOS

--- XamForms.Controls.Calendar.iOS/CalendarButtonRenderer.cs
+++ XamForms.Controls.Calendar.iOS/CalendarButtonRenderer.cs
@@ -77,7 +77,9 @@ namespace XamForms.Controls.iOS
 					var t = (int)Math.Ceiling(Control.Frame.Height * element.BackgroundPattern.GetTop(i));
 					var w = (int)Math.Ceiling(Control.Frame.Width * element.BackgroundPattern.Pattern[i].WidthPercent);
 					var h = (int)Math.Ceiling(Control.Frame.Height * element.BackgroundPattern.Pattern[i].HightPercent);
-					g.FillRect(new CGRect { X = l, Y = t, Width = w, Height = h });
+					var r = new CGRect { X = l, Y = t, Width = w, Height = h };
+					g.FillRect(r);
+                    DrawText(g, p, r);
 				}
 
 				image = UIGraphics.GetImageFromCurrentImageContext();
@@ -91,6 +93,41 @@ namespace XamForms.Controls.iOS
 		{
 			var handler = new FileImageSourceHandler();
 			return handler.LoadImageAsync(image);
+		}
+
+		protected void DrawText(CGContext g, Pattern p, CGRect r)
+		{
+			if (string.IsNullOrEmpty(p.Text)) return;
+			var bounds = p.Text.StringSize(UIFont.FromName("Helvetica",p.TextSize));
+			var al = (int)p.TextAlign;
+			var x = r.X;;
+
+			if ((al & 2) == 2) // center
+			{
+				x = r.X + (int)Math.Round(r.Width/2.0) - (int)Math.Round(bounds.Width / 2.0);
+			}
+			else if ((al & 4) == 4) // right
+			{
+				x = (r.X + r.Width) - bounds.Width - 2;
+			}
+			var y = r.Y + (int)Math.Round(bounds.Height/ 2.0) + 2;
+			if ((al & 16) == 16) // middle
+			{
+				y = r.Y + (int)Math.Ceiling(r.Height / 2.0) + (int)Math.Round(bounds.Height / 5.0);
+			}
+			else if ((al & 32) == 32) // bottom
+			{
+				y = (r.Y + r.Height) - 2;
+			}
+			Console.Out.WriteLine(r);
+			g.SaveState();
+			g.TranslateCTM(0, Bounds.Height);
+    		g.ScaleCTM(1,-1);
+			g.SetFillColor(p.TextColor.ToCGColor());
+			g.SetTextDrawingMode(CGTextDrawingMode.Fill);
+			g.SelectFont("Helvetica", p.TextSize, CGTextEncoding.MacRoman);
+			g.ShowTextAtPoint(x, Bounds.Height - y, p.Text);
+			g.RestoreState();
 		}
     }
 
